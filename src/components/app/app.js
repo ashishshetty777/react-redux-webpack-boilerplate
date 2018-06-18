91099d50b7e5c7bb4f839c3666c1b255983cf058
Ashish Shetty <ashishshetty777@gmail.com>
2018-06-18 15:45:10 +0530

    Initial setup done

---
+++ src/components/app/app.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import ReactDom from 'react-dom';
+import './app.scss';
+
+
+class App extends React.Component {
+
+	constructor(props){
+		super(props);
+	}
+
+	render(){		
+		return (
+			<h1> This is my app! </h1>
+			);
+	}
+}
+
+export default App;
