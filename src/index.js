a9fd422b099d817cf9c563376c675ca42432c0f0
Ashish Shetty <ashishshetty777@gmail.com>
2018-06-21 14:56:26 +0530

    Redux implementation

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import configureStore from './store/store';
 import App from './components/app/app';
 
+const store =  configureStore();
+
 ReactDOM.render(
-    <App />,
+	<Provider store={store}>
+    	<App />
+    </Provider>,
   document.querySelector('#app')
 );
